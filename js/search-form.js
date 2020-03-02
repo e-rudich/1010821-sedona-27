b981abca8c9acfc677c5cb4aac93f451da973042
Elena <e.d.rudich@gmail.com>
2020-03-02 23:38:04 +0300

    Оживление формы

---
+++ js/search-form.js
@@ -0,0 +1,26 @@
+var searchLink = document.querySelector(".pop-up-caller");
+var searchForm = document.querySelector(".pop-up-search-form");
+var form = searchForm.querySelector("form");
+var checkIn = searchForm.querySelector("[name=date-check-in]");
+var checkOut = searchForm.querySelector("[name=date-check-out]");
+var adultsNumber = searchForm.querySelector("[name=grown-ups]");
+
+if (searchForm) {
+    searchForm.classList.add("search-form-hide");
+}
+
+searchLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    searchForm.classList.remove("search-form-error");
+    searchForm.classList.toggle("search-form-hide");
+    checkIn.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!checkIn.value || !checkOut.value || !adultsNumber.value) {
+      evt.preventDefault();
+      searchForm.classList.remove("search-form-error");
+      searchForm.offsetWidth = searchForm.offsetWidth;
+      searchForm.classList.add("search-form-error");
+    }
+});
